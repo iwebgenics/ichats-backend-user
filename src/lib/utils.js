ca5761f008cda614711b39d75bc777901bd371b7
iwebgenics <developer@iwebgenics.com>
2025-03-07 15:30:54 +0530

    Initial commit or updated code

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,13 +4,13 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
-
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "none", 
-    secure: false, // so the cookie can be set over HTTP in local dev
+    sameSite: "lax", // This allows cookies for same-site requests
+    secure: true, // Must be true for HTTPS
   });
+  
   
 
   return token;
