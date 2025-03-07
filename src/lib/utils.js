4e325f47b48ea5511e0e2f1f143bd6f3da4d38af
iwebgenics <developer@iwebgenics.com>
2025-03-07 13:53:11 +0530

    Initial commit or updated code

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -7,10 +7,10 @@ export const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true,
-    sameSite: "strict",
-    secure: false, // Set secure to false because your frontend is running over HTTP
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    secure: true,
   });
-  
+
   return token;
 };
